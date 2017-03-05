17059529b0c4d9b64272dd3b15794acba4bd44e9
sean09 <tyang99@126.com>
2017-03-05 19:21:36 +0800

    add 分页功能

--- models/post.js
+++ models/post.js
@@ -1,12 +1,12 @@
 var mongodb = require('./db'),
     markdown = require('markdown').markdown;
 
-function Post(name, title, post) {
+function Post(name, title, tags, post) {
   this.name = name;
   this.title = title;
+  this.tags = tags;
   this.post = post;
-}
-
+};
 module.exports = Post;
 
 //存储一篇文章及其相关信息
@@ -26,8 +26,9 @@ Post.prototype.save = function(callback) {
       name: this.name,
       time: time,
       title: this.title,
+      tags: this.tags,
       post: this.post,
-      comments:[]
+      comments: []
   };
   //打开数据库
   mongodb.open(function (err, db) {
@@ -54,15 +55,15 @@ Post.prototype.save = function(callback) {
   });
 };
 
-//读取文章及其相关信息
-Post.getAll = function(name, callback) {
+//一次获取十篇文章
+Post.getTen = function(name, page, callback) {
   //打开数据库
   mongodb.open(function (err, db) {
     if (err) {
       return callback(err);
     }
     //读取 posts 集合
-    db.collection('posts', function(err, collection) {
+    db.collection('posts', function (err, collection) {
       if (err) {
         mongodb.close();
         return callback(err);
@@ -71,19 +72,25 @@ Post.getAll = function(name, callback) {
       if (name) {
         query.name = name;
       }
-      //根据 query 对象查询文章
-      collection.find(query).sort({
-        time: -1
-      }).toArray(function (err, docs) {
-        mongodb.close();
-        if (err) {
-          return callback(err);//失败！返回 err
-        }
-	//解析markdown为html
-	docs.forEach(function(doc) {
-	    doc.post = markdown.toHTML(doc.post);
-	});
-        callback(null, docs);//成功！以数组形式返回查询的结果
+      //使用 count 返回特定查询的文档数 total
+      collection.count(query, function (err, total) {
+        //根据 query 对象查询，并跳过前 (page-1)*10 个结果，返回之后的 10 个结果
+        collection.find(query, {
+          skip: (page - 1)*10,
+          limit: 10
+        }).sort({
+          time: -1
+        }).toArray(function (err, docs) {
+          mongodb.close();
+          if (err) {
+            return callback(err);
+          }
+          //解析 markdown 为 html
+          docs.forEach(function (doc) {
+            doc.post = markdown.toHTML(doc.post);
+          });
+          callback(null, docs, total);
+        });
       });
     });
   });
@@ -206,6 +213,93 @@ Post.remove = function(name, day, title, callback) {
           return callback(err);
         }
         callback(null);
+      });
+    });
+  });
+};
+
+
+//返回所有文章存档信息
+Post.getArchive = function(callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    //读取 posts 集合
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //返回只包含 name、time、title 属性的文档组成的存档数组
+      collection.find({}, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+//返回含有特定标签的所有文章
+Post.getTags = function(callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //distinct 用来找出给定键的所有不同值
+      collection.distinct("tags", function (err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+//返回特定标签页
+Post.getTag = function(tag, callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //查询所有 tags 数组内包含 tag 的文档
+      //并返回只含有 name、time、title 组成的数组
+      collection.find({
+        "tags": tag
+      }, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, docs);
       });
     });
   });
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,24 @@ var crypto = require('crypto'),
     Comment = require('../models/comment.js');
 
 module.exports = function (app) {
-  app.get('/',function (req,res) {
-    Post.getAll(null,function(err,posts) {
-      if(err) {
-      	posts = [];
-      }
-      res.render('index',{
-        title:'主页',
-        user:req.session.user,
-        posts:posts,
-        success:req.flash('success').toString(),
-        error:req.flash('error').toString()
-      });
-    });
-  });
+  app.get('/', function (req, res) {
+		var page = parseInt(req.query.p) || 1;
+		Post.getTen(null, page, function(err, posts, total){
+			if(err) {
+				posts = [];
+			}
+			res.render('index', {
+				title: '主页',
+				page: page,
+				isFirstPage: (page-1) == 0,
+				isLastPage: ((page - 1) * 10 + posts.length) == total,
+				user: req.session.user,
+				posts: posts,
+				success: req.flash('success').toString(),
+				error: req.flash('error').toString()
+			});
+		});
+	});
     app.get('/reg', checkNotLogin);
     app.get('/reg',function(req,res) {
     	res.render('reg',{
@@ -112,8 +116,9 @@ module.exports = function (app) {
     });
     app.post('/post', checkLogin);
     app.post('/post', function (req, res) {
-	var currentUser = req.session.user,
-	    post = new Post(currentUser.name,req.body.title,req.body.post);
+      var currentUser = req.session.user,
+        tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+        post = new Post(currentUser.name, req.body.title, tags, req.body.post);
 	post.save(function(err) {
 	    if(err) {
 		req.flash('error',err);
@@ -145,27 +150,79 @@ module.exports = function (app) {
     });
 
     app.get('/u/:name', function (req, res) {
+      var page = req.query.p ? parseInt(req.query.p) : 1;
       //检查用户是否存在
       User.get(req.params.name, function (err, user) {
         if (!user) {
           req.flash('error', '用户不存在!');
-          return res.redirect('/');//用户不存在则跳转到主页
-        }
-      //查询并返回该用户的所有文章
-      Post.getAll(user.name, function (err, posts) {
-        if (err) {
-          req.flash('error', err);
           return res.redirect('/');
         }
-        res.render('user', {
-          title: user.name,
-          posts: posts,
-          user : req.session.user,
-          success : req.flash('success').toString(),
-          error : req.flash('error').toString()
+        //查询并返回该用户第 page 页的 10 篇文章
+        Post.getTen(user.name, page, function (err, posts, total) {
+          if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+          }
+          res.render('user', {
+            title: user.name,
+            posts: posts,
+            page: page,
+            isFirstPage: (page - 1) == 0,
+            isLastPage: ((page - 1) * 10 + posts.length) == total,
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+          });
         });
       });
     });
+
+  app.get('/archive', function (req, res) {
+    Post.getArchive(function (err, posts) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      res.render('archive', {
+        title: '存档',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+  app.get('/tags', function (req, res) {
+    Post.getTags(function (err, posts) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      res.render('tags', {
+        title: '标签',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+app.get('/tags/:tag', function (req, res) {
+  Post.getTag(req.params.tag, function (err, posts) {
+    if (err) {
+      req.flash('error',err);
+      return res.redirect('/');
+    }
+    res.render('tag', {
+      title: 'TAG:' + req.params.tag,
+      posts: posts,
+      user: req.session.user,
+      success: req.flash('success').toString(),
+      error: req.flash('error').toString()
+    });
+  });
 });
 
 app.get('/u/:name/:day/:title', function (req, res) {

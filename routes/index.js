1fd74d0f74046852b6b4e18b15d817c3906587dd
sean09 <tyang99@126.com>
2017-03-06 21:47:02 +0800

    初步完成

--- routes/index.js
+++ routes/index.js
@@ -149,6 +149,22 @@ module.exports = function (app) {
   	   res.redirect('/upload');
     });
 
+    app.get('/search', function (req, res) {
+      Post.search(req.query.keyword, function (err, posts) {
+        if (err) {
+          req.flash('error', err);
+          return res.redirect('/');
+        }
+        res.render('search', {
+          title: "SEARCH:" + req.query.keyword,
+          posts: posts,
+          user: req.session.user,
+          success: req.flash('success').toString(),
+          error: req.flash('error').toString()
+        });
+      });
+    });
+
     app.get('/u/:name', function (req, res) {
       var page = req.query.p ? parseInt(req.query.p) : 1;
       //检查用户是否存在
@@ -307,6 +323,7 @@ app.get('/remove/:name/:day/:title', function (req, res) {
     res.redirect('/');
   });
 });
+
 
 //权限控制，检测已登录状态
 function checkNotLogin(req,res,next) {
